2f81b3f5e2fb4628c7faae763a916c27c0190fce
davidddfm <davidflores@Davids-MacBook-Pro.local>
2020-05-10 20:15:21 -0500

    ajuste de exportacion

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 //base64
-function convertirBase64(str) {
-  return Buffer.from(str).toString("base64");
-}
+const convertirBase64 = (str) => Buffer.from(str).toString("base64");
 
 //ROT13
-function convertirROT13(str) {
+const convertirROT13 = (str) => {
   return str.replace(/[a-zA-Z]/g, function (c) {
     return String.fromCharCode(
       (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26
     );
   });
-}
+};
 
 //HEX
-function convertirHEX(str) {
+const convertirHEX = (str) => {
   let arr1 = [];
   for (let n = 0, l = str.length; n < l; n++) {
     let hex = Number(str.charCodeAt(n)).toString(16);
     arr1.push(hex);
   }
   return arr1.join("");
-}
+};
 
 //Binario
-function convertirBin(str) {
+const convertirBin = (str) => {
   let res = "";
   for (let i = 0; i < str.length; i++) {
     let e = str[i].charCodeAt(0);
@@ -40,7 +38,6 @@ function convertirBin(str) {
   }
 
   return res;
-}
+};
 
-
-export { convertirBase64, convertirROT13, convertirHEX, convertirBin};
+export { convertirBin, convertirHEX, convertirROT13, convertirBase64 };
